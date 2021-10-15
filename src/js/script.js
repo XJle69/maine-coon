31962ddf1c2ce24039ff81048e44c4d1b3af9534
XJle69 <vladkrupa88@gmail.com>
2021-10-15 13:14:07 +0300

    navbar ready&optimazed JS

--- src/js/script.js
+++ src/js/script.js
@@ -2,35 +2,70 @@ var modal = document.querySelector(".modal");
 var closeBtn = modal.querySelector(".modal__close");
 var modalBtn = document.querySelector(".ques__btn");
 var question = document.querySelectorAll(".faq__question");
+var burger = document.querySelector(".navbar__burger");
+var navbarMenu = document.querySelector(".navbar-menu");
+
 var i;
 
 modalBtn.addEventListener("click", openModal);
-closeBtn.addEventListener("click", closeModal);
-window.addEventListener("click", outsideClick);
+burger.addEventListener("click", openCloseNavbar);
 
-if (question) {
-  for (i = 0; i < question.length; i++) {
-    question[i].addEventListener("click", openAnswer);
-  }
+for (i = 0; i < question.length; i++) {
+  question[i].addEventListener("click", openAnswer);
 }
+
+function init() {
+  burger.classList.toggle("navbar__burger--none");
+  navbarMenu.classList.toggle("navbar__menu--none");
+}
+
+init();
 
 function openAnswer(env) {
   this.classList.toggle("active-btn");
   this.nextElementSibling.classList.toggle("faq__answer--show");
 }
 
+function addCloseListner() {
+  window.addEventListener("keydown", escapeCloseModal);
+  window.addEventListener("click", outsideClick);
+  closeBtn.addEventListener("click", closeModal);
+}
+
+function removeCloseListner() {
+  window.removeEventListener("keydown", escapeCloseModal);
+  window.removeEventListener("click", outsideClick);
+  closeBtn.removeEventListener("click", closeModal);
+}
+
 function openModal(evt) {
   evt.preventDefault();
-  modal.style.display = "flex";
+  modal.classList.toggle("modal--hide");
+  addCloseListner();
 }
 
 function closeModal(evt) {
   evt.preventDefault();
-  modal.style.display = "none";
+  modal.classList.toggle("modal--hide");
+  removeCloseListner();
 }
 
-function outsideClick(e) {
-  if (e.target == modal) {
-    modal.style.display = "none";
+function escapeCloseModal(evt) {
+  if (evt.key === "Escape") {
+    modal.classList.toggle("modal--hide");
+    removeCloseListner();
   }
+}
+
+function outsideClick(evt) {
+  if (evt.target == modal) {
+    modal.classList.toggle("modal--hide");
+    removeCloseListner();
+  }
+}
+
+function openCloseNavbar(evt) {
+  evt.preventDefault();
+  navbarMenu.classList.toggle("navbar__menu--none");
+  burger.classList.toggle("navbar__burger--active");
 }
